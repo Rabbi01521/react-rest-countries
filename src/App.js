906029e2d61a8dccdafcae21ff3345e7192dd43b
Rabbi01521 <ishan0563@gmail.com>
2021-09-20 02:09:49 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import "./App.css";
 import Countries from "./components/Countries/Countries";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Countries></Countries>
+      <Footer></Footer>
     </div>
   );
 }
 
-/* function Countries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch("https://restcountries.eu/rest/v2/all")
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
-  return (
-    <div>
-      <h1>Travaling Around The World!!!</h1>
-      <h4>Available Found: {countries.length}</h4>
-    </div>
-  );
-} */
 export default App;
